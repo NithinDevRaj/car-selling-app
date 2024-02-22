1bef32e63bd4311b7081afa81fad35952996ec5d
Nithin Raj S <120079923+NithinDevRaj@users.noreply.github.com>
2024-02-22 16:00:08 +0530

    bug fic

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -3,7 +3,7 @@ import errorHandler from "../utils/error.handler.js";
 
 export const createListing = async (req, res, next) => {
   try {
-    console.log(req.body)
+  
     const listing = await Listing.create(req.body);
     return res.status(201).json(listing);
   } catch (error) {
@@ -31,7 +31,9 @@ export const deleteListing = async (req, res, next) => {
 };
 
 export const updateListing = async (req, res, next) => {
+  console.log(req.body,req.params)
   const listing = await Listing.findById(req.params.id);
+  console.log(listing)
   if (!listing) {
     return next(errorHandler(404, "Listing not found!"));
   }
