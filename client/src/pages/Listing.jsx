ee1c9e6b13aa2714aa256e91ab997a2864bb0cb6
nithinDevRaj <nithinraj.dev@gmail.com>
2024-05-12 10:53:19 +0530

    double

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -139,7 +139,7 @@ export default function Listing() {
                   onClick={() => setContact(true)}
                   className='bg-lime-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
                 >
-                  Contact landlord
+                  Contact 
                 </button>
               )}
               {contact && <Contact listing={listing} />}
